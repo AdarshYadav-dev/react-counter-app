a07cca163b27d4bb4ba44a434cc03b91997b5cfe
AdarshYadav-dev <adarsh98215@gmail.com>
2025-09-10 22:22:42 +0530

    Initial commit - React Counter App

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+// import './Components/Counter';
+import Counter from './Components/Counter';
+
+function App() {
+
+
+  return (
+    <>
+    <Counter />
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Counter.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Counter.css";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function Increment() {
+    setCount(count + 1);
+  }
+
+  function Decrement() {
+    setCount(count - 1);
+  }
+
+  function Reset() {
+    setCount(0);
+  }
+
+  return (
+    <>
+      <h1 className="title">Counter Application !!</h1>
+      <h2 className="count">{count}</h2>
+
+      <div className="btn-group">
+        <button onClick={Increment}>Increment</button>
+        <button onClick={Reset}>Reset</button>
+        <button onClick={Decrement}>Decrement</button>
+      </div>
+    </>
+  );
+}
+
+export default Counter;
